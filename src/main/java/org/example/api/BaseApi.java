ddcda0c800320f6277a75d9aae9ad75f58941338
Yuri Pankrushin <pankrushinyuri@mail.ru>
2023-02-27 23:07:54 +0300

    remove /

--- src/main/java/org/example/api/BaseApi.java
+++ src/main/java/org/example/api/BaseApi.java
@@ -6,5 +6,5 @@ import io.restassured.specification.RequestSpecification;
 public class BaseApi {
     public RequestSpecification requestSpecification =
             RestAssured.given()
-                    .baseUri("https://stellarburgers.nomoreparties.site/");
+                    .baseUri("https://stellarburgers.nomoreparties.site");
 }
